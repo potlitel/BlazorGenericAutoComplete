ffe63cd116417aff05fab907b0744071ddc6746d
potlitel <potlitel@gmail.com>
2023-11-01 17:05:31 -0400

    Se completa funcionalidad de filtrado incluyendo uso de json-server.

---
+++ BlazorGenericAutoComplete/Models/Tarea.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorGenericAutoComplete.Models
+{
+    public class Tarea
+    {
+        public int id { get; set; }
+
+        public string name { get; set; } = null!;
+
+        public string description { get; set; } = null!;
+
+        public bool completed { get; set; }
+    }
+}
